dd164bec36b50171a8fa1f12134f97501f5b73de
joaolobao380 <victor_jv380@hotmail.com>
2020-08-27 17:19:05 -0300

    final

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance
+    }); 
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +25,23 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    //receive user information
+    const { title, value, type } = request.body;
+
+    //instance new transaction
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    )
+    // create new transaction, passing values title, value and type. 
+    // values passed through request
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type
+    })
+
+    return response.json(transaction);
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
